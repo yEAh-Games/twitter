9bea7b6036c019d66b105ad96516fe7dce8c6706
yEAh-Games <tallinnquinton@gmail.com>
2023-05-22 20:56:39 -0400

    Redoing format

--- _includes/app.js
+++ _includes/app.js
@@ -1,7 +1,4 @@
-{% assign username = page.name | remove: ".md" %}
-{% assign account = site.data.accounts | where: "username", username | first %}
-
-fetch('https://archive{{account.host}}.yeahgames.net/c/artifacts/twitter/{{account.username}}/tweets.jsonl')
+fetch('tweets.jsonl')
     .then(response => response.text())
     .then(data => {
         const tweets = data.trim().split('\n');
@@ -141,3 +138,25 @@ fetch('https://archive{{account.host}}.yeahgames.net/c/artifacts/twitter/{{accou
     .catch(error => console.error(error));
 
 
+    {% assign username = page.name | remove: ".md" %}
+    {% assign account = site.data.accounts | where: "username", username | first %}
+    
+    
+    var progressBar = document.querySelector('#progress-bar');
+    var request = new XMLHttpRequest();
+    var url = 'https://archive{{account.host}}.yeahgames.net/c/artifacts/twitter/{{account.username}}/tweets.jsonl';
+    request.open('GET', url, true);
+    
+    request.addEventListener('progress', function (event) {
+      if (event.lengthComputable) {
+        var percentComplete = (event.loaded / event.total) * 100;
+        progressBar.style.width = percentComplete + '%';
+      }
+    });
+    
+    request.addEventListener('load', function (event) {
+      // File has been successfully loaded
+      progressBar.style.width = '100%';
+    });
+    
+    request.send();
